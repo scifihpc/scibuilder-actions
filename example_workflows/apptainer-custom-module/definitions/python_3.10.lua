4c80c2601dfa3fe6adaa6e17f1c64f4c33ab637b
Simo Tuomisto <simo.tuomisto@aalto.fi>
2024-05-17 12:53:18 +0300

    apptainer-build: Adding a new builder for apptainer build

---
+++ example_workflows/apptainer-custom-module/definitions/python_3.10.lua
@@ -0,0 +1,10 @@
+-- -*- lua -*-"
+--
+-- Module file created by scibuilder
+--
+
+whatis([[Name : __name__]])
+whatis([[Version : __version__]])
+help([[This is an automatically created apptainer image with custom module.]])
+
+setenv("IMAGE_PATH", "__image__")
